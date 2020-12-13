ac73ea18c48024ab16dcbb47ee3201d6f638a4f4
lukas <lukas.zizas@gmail.com>
2020-12-13 11:53:25 +0200

    ww commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+import requests
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # response = requests.get("http://www.google.lt")
+    # print(response.status_code)
+    pass
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+
+def two_numbers_addition(a, b):
+    suma = a + b
+    return suma
+
+def numbers_substraction(a, b):
+    """subtracts numbers """
+    substraction = a - b
+    return substraction
+
+def any_function():
+    return "anything"
+
+# print(sum([1, 2]))
+
